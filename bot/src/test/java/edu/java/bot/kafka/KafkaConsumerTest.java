b89e3c67eb93aef58f3820dfae53fd7cd751e767
artseroff <artseroff@gmail.com>
2024-04-08 01:05:41 +0300

    hw8: add dlq tests

--- bot/src/test/java/edu/java/bot/kafka/KafkaConsumerTest.java
+++ bot/src/test/java/edu/java/bot/kafka/KafkaConsumerTest.java
@@ -1,5 +1,6 @@
 package edu.java.bot.kafka;
 
+import edu.java.bot.service.kafka.dlq.DeadLetterQueue;
 import edu.java.bot.service.link.LinkUpdatesHandler;
 import edu.java.request.LinkUpdateRequest;
 import java.net.URI;
@@ -14,22 +15,38 @@ import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 public class KafkaConsumerTest extends KafkaIntegrationTest {
+    private final KafkaTemplate<String, LinkUpdateRequest> linkUpdateKafkaTemplate;
+    private final KafkaTemplate<String, String> stringKafkaTemplate;
+
+    @Value("${kafka.scrapper-topic}")
+    private String scrapperTopicName;
+
+    @Value("${kafka.dlq-topic}")
+    private String dlqTopic;
+
+    @MockBean
+    private DeadLetterQueue deadLetterQueue;
+
     @MockBean
     private LinkUpdatesHandler linkUpdatesHandler;
 
-    private final KafkaTemplate<String, LinkUpdateRequest> linkUpdateKafkaTemplate;
-
     @Autowired
-    public KafkaConsumerTest(KafkaTemplate<String, LinkUpdateRequest> linkUpdateKafkaTemplate) {
+    public KafkaConsumerTest(
+        KafkaTemplate<String, LinkUpdateRequest> linkUpdateKafkaTemplate,
+        KafkaTemplate<String, String> stringKafkaTemplate
+    ) {
         this.linkUpdateKafkaTemplate = linkUpdateKafkaTemplate;
+        this.stringKafkaTemplate = stringKafkaTemplate;
     }
 
     @Test
-    public void botSendUpdatesToChatTest(@Value("${kafka.scrapper-topic}") String scrapperTopicName) {
+    public void botSendUpdatesToChatTest() {
 
+        // Arrange
         LinkUpdateRequest linkUpdateRequest =
             new LinkUpdateRequest(1L, URI.create("https://github.com/artseroff/Link-tracker"), "", List.of(1L));
 
+        // Act
         linkUpdateKafkaTemplate.send(scrapperTopicName, linkUpdateRequest);
 
         try {
@@ -38,6 +55,74 @@ public class KafkaConsumerTest extends KafkaIntegrationTest {
             throw new RuntimeException(e);
         }
 
+        // Assert
         Mockito.verify(linkUpdatesHandler).processUpdate(linkUpdateRequest);
+    }
+
+    @Test
+    public void notValidLinkUpdateRedirectToDlqTest() {
+
+        // Arrange
+        LinkUpdateRequest linkUpdateRequest =
+            new LinkUpdateRequest(null, null, null, null);
+
+        // Act
+        linkUpdateKafkaTemplate.send(scrapperTopicName, linkUpdateRequest);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Assert
+        Mockito.verifyNoInteractions(linkUpdatesHandler);
+        Mockito.verify(deadLetterQueue).send(Mockito.any());
+    }
+
+    @Test
+    public void cantParseJsonToLinkUpdateRedirectToDlqTest() {
+
+        // Arrange
+        String noLinkUpdateRequestJson = """
+            {
+               "message": "Not Found",
+               "documentation_url": "https://docs.github.com/rest/repos/repos#get-a-repository"
+             }
+            """;
+
+        // Act
+        stringKafkaTemplate.send(scrapperTopicName, noLinkUpdateRequestJson);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Assert
+        Mockito.verifyNoInteractions(linkUpdatesHandler);
+        Mockito.verify(deadLetterQueue).send(Mockito.any());
+    }
+
+    @Test
+    public void anyExceptionDuringSendingToChatRedirectToDlqTest() {
+
+        // Arrange
+        LinkUpdateRequest linkUpdateRequest =
+            new LinkUpdateRequest(1L, URI.create("https://github.com/artseroff/Link-tracker"), "", List.of(1L));
+        Mockito.doThrow(RuntimeException.class).when(linkUpdatesHandler).processUpdate(Mockito.any());
+
+        // Act
+        linkUpdateKafkaTemplate.send(scrapperTopicName, linkUpdateRequest);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Assert
+        Mockito.verify(deadLetterQueue).send(Mockito.any());
     }
 }
