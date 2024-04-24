c0cfd7a414fa39c6c6722bc26ba716d38630ea8c
artseroff <artseroff@gmail.com>
2024-04-24 23:02:40 +0300

    remove commented out code

--- scrapper/src/main/java/edu/java/scrapper/service/updater/sender/LinkUpdatesSender.java
+++ scrapper/src/main/java/edu/java/scrapper/service/updater/sender/LinkUpdatesSender.java
@@ -4,15 +4,4 @@ import edu.java.request.LinkUpdateRequest;
 
 public interface LinkUpdatesSender {
     void sendUpdates(LinkUpdateRequest updateRequest);
-    /*private final ApplicationConfig config;
-    private final BotClient botClient;
-    private final ScrapperQueueProducer queueProducer;
-
-    public void sendUpdates(LinkUpdateRequest updateRequest) {
-        if (config.useQueue()) {
-            queueProducer.send(updateRequest);
-        } else {
-            botClient.updates(updateRequest);
-        }
-    }*/
 }
